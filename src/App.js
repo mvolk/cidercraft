d674f2c119a0e2a040097c67b17a02c534b60ae2
Michael Volk <michael-volk@hotmail.com>
2020-10-28 13:39:17 -0700

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 
 function App() {
   return (
@@ -22,4 +25,52 @@ function App() {
   );
 }
 
-export default App;
+const signUpFormFields = [
+  {
+    type: 'name',
+    label: 'Name *',
+    placeholder: 'Enter your name',
+    required: true,
+  },
+  {
+    type: 'email',
+    label: 'Email *',
+    required: true,
+  },
+  {
+    type: 'password',
+    label: 'Password *',
+    required: true,
+  },
+];
+
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+          setAuthState(nextAuthState);
+          setUser(authData)
+      });
+  }, []);
+
+return authState === AuthState.SignedIn && user ? (
+    <App />
+  ) : (
+    <AmplifyAuthenticator
+      initialAuthState="signup"
+      usernameAlias="email"
+    >
+      <AmplifySignUp
+        slot="sign-up"
+        headerText="Create a free account"
+        usernameAlias="email"
+        formFields={signUpFormFields}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
+);
+}
+
+export default AuthStateApp;
